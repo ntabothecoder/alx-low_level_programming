3a8efccbd549cad4b00671aa6796b885c0bcf884
ntabothecoder <benntabok@gmail.com>
2022-09-12 21:36:17 +0300

    4. When I was having that alphabet soup, I never thought that it would pay off'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - print if the number is positive or negative
+* [A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C zero or negative[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+*using the main function
+[A[CDescription- Using the main function
+*this program prints 'programming is positive, zero or negative
+*Return: 0
+*/
+int main(void) 
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+{
+	char ch;
+	for (ch = 'a'; ch<= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+
+[A[C[C[C[C[C[C[C[C[	}
+	}
+putchar("\n");
+return(0);
+}
