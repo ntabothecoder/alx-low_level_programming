b8683e32f006a295d0f05fe322e90ed18398f8a9
ntabothecoder <benntabok@gmail.com>
2022-09-12 02:20:54 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+ * main: Prints the alphabet in lowercase & uppercase
+ *
+ * DEscription: This program prints the alphabet 
+ * in lowercase and then uppercase followed by
+ * a new line
+ * return: 0
+ */
+int maint(void)
+{
+	char ch;
+	for (ch = 'a', ch <='z', ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A', ch<= 'Z', ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
