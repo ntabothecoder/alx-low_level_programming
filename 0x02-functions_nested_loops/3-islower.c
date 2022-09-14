81937c0581f198cdbb263a2b78dc6937cd6ddb10
ntabothecoder <benntabok@gmail.com>
2022-09-15 02:16:18 +0300

    Added four files

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks whether a letter is lowercase or  not
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+		return(0);
+}
