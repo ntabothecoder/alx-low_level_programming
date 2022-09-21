a77cf3ae5579eeb033e67cc8bc3b1a8232dcd00b
ntabothecoder <benntabok@gmail.com>
2022-09-21 04:14:20 +0300

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <main.h>
+
+/**
+ * reset_to_98 - resets the value of the given int to 98
+ * @n : parameter
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypes()
+ * int prototypes ()
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *n);
+int _strlen(char *s);
+void _puts(cha *s);
+void print_rev(char *s);
+void re_string(char *str);
+void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+
+endif /* MAIN_H */
