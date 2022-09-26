27b722d400e50dee2e51a8c961f5a77f9a7db02c
ntabothecoder <benntabok@gmail.com>
2022-09-26 18:23:22 +0300

    Task 0, main.h, README.md

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * memset - fills memory with a constant byte
+ *
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+	
+	for (i = 0; n > 0; i++; n--)
+	{
+		s[i] = b;
+	}
+	return s;
+}
+
