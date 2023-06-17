fed9145792a759479e57b1b1472eb581bcd9c99d
Ben Ntabo <benntabok@gmail.com>
2023-06-18 02:18:56 +0300

    Hell, print f

---
+++ 0x00-hello_world/5-print_f.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art.\n");
+	return (0);
+}
+
