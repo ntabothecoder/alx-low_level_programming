e929ce902b076c3a48e0d8a676dfc635f582780e
Ben Ntabo <benntabok@gmail.com>
2023-06-18 02:08:21 +0300

    Optimised 4-puts for betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - This is the entry point of the program
+ * main - Entry point of the program
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (indicating success)
  */
-int main() 
+int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return {0};
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
 
