563f587746a97a2b861b348f67546e4b899c868b
Ben Ntabo <benntabok@gmail.com>
2023-06-19 11:08:22 +0300

    Positive anything is better than negative nothing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various types.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints the size of various types.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
