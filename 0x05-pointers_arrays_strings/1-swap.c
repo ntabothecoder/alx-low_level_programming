fe2210f9142a4939b1a1717eb8f264548a42d0b1
Ben Ntabo <benntabok@gmail.com>
2023-06-26 10:56:25 -0700

    2-strlen

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the value sof two integers
- * @a - first int
- * @b - second int
- * Returns: Nothing
+ * swap_int - swaps the values of two integers
+ *
+ * @a - the  first int
+ *@b - the second int
+ *
+ *Returns: Nothing
  */
 
 void swap_int(int *a, int *b)
