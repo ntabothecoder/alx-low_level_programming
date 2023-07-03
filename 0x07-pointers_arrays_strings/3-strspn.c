13844fc0faac9818f74fa833497c6c30fe3bb434
Ben Ntabo <benntabok@gmail.com>
2023-07-04 01:07:38 +0300

    Compiled all tasks

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,29 @@
-#include"stdio.h"
-
+#include "main.h"
 /**
-*strspn-getthelengnthofaprefixstring
-*
-*Return:s
-*/
-unsignedint_strspn(char*s,char*accept)
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
 {
-	unsignedintlength=0;
-	intfound=0;
+	unsigned int n = 0;
+	int r;
 
-	while(*s!='\0')
+	while (*s)
 	{
-		found=0;
-
-	for(char*a=accept;*a!='\0';a++)
-	{
-	if(*s==*a)
-	{
-		found=1;
-		break;
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
 	}
-	}	
-	if(!found)
-	{
-	break;
-	}
-
-	length++;
-	s++;
-	}
-	return(length);
+	return (n);
 }
 
