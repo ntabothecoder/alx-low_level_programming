ec719ee706256c842483c6ca6e69bef4d0a946b4
Ben Ntabo <benntabok@gmail.com>
2023-07-03 20:39:48 +0300

    memset,c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,11 +6,11 @@
  *
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 	
-	for (i = 0; n > 0; i++; n--)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
 	}
