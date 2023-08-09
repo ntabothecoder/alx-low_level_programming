72dac1f47e06c55b29b8eb5c1faa88006740c3b2
Ben Ntabo <benntabok@gmail.com>
2023-08-09 12:55:52 -0700

    Coin task

---
+++ 0x0A-arg_argv/0-whatsmyname.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdlib.h>
+
+int main(argc, **argv[])
+{
+	printf("%s\n", argv[0]);
+	printf
---
+++ 0x0A-arg_argv/1-args.c
@@ -0,0 +1,22 @@
+1-args.c
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
