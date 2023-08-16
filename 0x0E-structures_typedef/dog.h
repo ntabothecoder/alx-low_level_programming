e705c5397e22693ce87594c9472560990946de73
Ben Ntabo <benntabok@gmail.com>
2023-08-16 10:40:50 -0700

    created struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * struct dog - a strcut about a dog
+ * @owner : dogs owner
+ * @age: dogs age
+ * @name: dogs name
+ */
+
+struct Dog {
+	char *owner;
+	float age;
+	char *name;
+};
